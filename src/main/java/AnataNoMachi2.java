ad7f16b64c5adbff0a40af847a8a3b409fe7f1ad
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2012-09-04 22:01:18 +0900

    9/4 22:01

--- src/main/java/AnataNoMachi2.java
+++ src/main/java/AnataNoMachi2.java
@@ -26,21 +26,36 @@ public class AnataNoMachi2 {
 		System.out.println(taro.getKeiyaku() + " " + taro.getRyokin());
 
 		// 電話番号を表示する 項番6
-		// 電話番号を変更する 項番7
-		// 電話番号を表示する 項番8
-
-		System.out.print("インスタンス[taro]の情報：");
-		System.out.println(taro.KAISYA + " " + taro.getKataban() + " "
-				+ taro.getRyokin() + " " + taro.getBangou() + " "
-				+ taro.getRyokin());
-
 		System.out.print("インスタンス[taro]の情報（変更前）：");
 		System.out.println(taro.getKeiyaku() + " " + taro.getBangou());
 
+		// 電話番号を変更する 項番7
 		taro.setBangou("050-7500-1588");
 
+		// 電話番号を表示する 項番8
 		System.out.print("インスタンス[taro]の情報（変更後）：");
 		System.out.println(taro.getKeiyaku() + " " + taro.getBangou());
+
+		// 内線をかける 項番9
+		for (int i = 1; i <= 5; i++) {
+			taro.naisen(i);
+		}
+
+		// 子機の数を表示する 項番10
+		System.out.print("子機の数（変更前）：");
+		System.out.println(" " + taro.getKokiSu());
+
+		// 子機の数を変更する 項番11
+		taro.setKokiSu(10);
+
+		// 子機の数を表示する 項番12
+		System.out.print("子機の数（変更後）：");
+		System.out.println(" " + taro.getKokiSu());
+
+		// 会社名、型番号、契約者名を表示する 項番13
+		System.out.print("インスタンス[taro]の情報：");
+		System.out.println(taro.KAISYA + " " + taro.getKataban() + " "
+				+ taro.getKeiyaku());
 
 	}
 
--- src/main/java/pack1/KokiDenwa.java
+++ src/main/java/pack1/KokiDenwa.java
@@ -7,13 +7,14 @@ public class KokiDenwa extends Denwa {
 	public KokiDenwa(String keiyaku, String bangou, int kokisu) {
 		super(keiyaku, bangou);
 		this.kokiSu = kokisu;
+		super.kataban = "KOKI-001";
 	}
 
-	void naisen(int kokiBangou) {
-		if (kokiBangou > this.kokiSu) {
+	public void naisen(int kokiBangou) {
+		if (kokiBangou > this.kokiSu || kokiBangou < 1) {
 			System.out.println("その番号の子機はありません");
 		} else {
-			System.out.println(kokiBangou + "の子機と内線通話しました");
+			System.out.println(kokiBangou + "番の子機と内線通話しました");
 		}
 	}
 
