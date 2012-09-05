934702c73510b48fbde10c2ff9c389f54970a1e4
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2012-09-05 23:17:25 +0900

    9/5 23:17

--- src/main/java/AnataNoMachi3.java
+++ src/main/java/AnataNoMachi3.java
@@ -10,50 +10,56 @@ public class AnataNoMachi3 {
 		KeitaiDenwa taro = new KeitaiDenwa("太郎", "000-000-0000",
 				"yamauchi@std.it-college.ac.jp");
 
-		// 何回か電話する 項番２
-		for (int i = 0; i < 3; i++) {
-			taro.denwaSuru();
-		}
-
-		// 電話料金表示 項番３
-		System.out.print("インスタンス[taro]の情報：");
-		System.out.println(taro.getKeiyaku() + " " + taro.getRyokin());
-
-		// 電話料金クリア 項番４
-		taro.clearRyokin();
-
-		// 電話料金表示 項番５
-		System.out.print("インスタンス[taro]の情報：");
-		System.out.println(taro.getKeiyaku() + " " + taro.getRyokin());
+		// // 何回か電話する 項番２
+		// for (int i = 0; i < 3; i++) {
+		// taro.denwaSuru();
+		// }
+		//
+		// // 電話料金表示 項番３
+		// System.out.print("インスタンス[taro]の情報：");
+		// System.out.println(taro.getKeiyaku() + " " + taro.getRyokin());
+		//
+		// // 電話料金クリア 項番４
+		// taro.clearRyokin();
+		//
+		// // 電話料金表示 項番５
+		// System.out.print("インスタンス[taro]の情報：");
+		// System.out.println(taro.getKeiyaku() + " " + taro.getRyokin());
 
 		// 何回かメールを送信する 項番6
-		for (int i = 0; i < 5; i++) {
-			taro.mail();
-		}
+		// for (int i = 0; i < 5; i++) {
+		// taro.mail();
+		// }
+
+		taro.mail();
+		taro.mail("send");
+		taro.mail("receive");
+		taro.mail("send", 1000);
+		taro.mail("receive", 50);
 
 		// パケット料金を表示する 項番7
 		System.out.print("パケット料金：");
 		System.out.println(taro.getPacketRyokin());
 
-		// 変更前のメールアドレスを表示する 項番8
-		System.out.print("メールアドレス（変更前）：");
-		System.out.println(taro.getMailAddress());
-
-		// メールアドレスを変更する 項番9
-		taro.setMailAddress("kimiyuki.yamauchi.0313@gmail.com");
-
-		// 変更後のメールアドレスを表示する 項番10
-		System.out.print("メールアドレス（変更後）：");
-		System.out.println(taro.getMailAddress());
-
-		// 電話番号を表示する 項番11
-		System.out.print("電話番号：");
-		System.out.println(taro.getBangou());
-
-		// 会社名、型番号、契約者名を表示する 項番12
-		System.out.print("インスタンス[taro]の情報：");
-		System.out.println(taro.KAISYA + " " + taro.getKataban() + " "
-				+ taro.getKeiyaku());
+		// // 変更前のメールアドレスを表示する 項番8
+		// System.out.print("メールアドレス（変更前）：");
+		// System.out.println(taro.getMailAddress());
+		//
+		// // メールアドレスを変更する 項番9
+		// taro.setMailAddress("kimiyuki.yamauchi.0313@gmail.com");
+		//
+		// // 変更後のメールアドレスを表示する 項番10
+		// System.out.print("メールアドレス（変更後）：");
+		// System.out.println(taro.getMailAddress());
+		//
+		// // 電話番号を表示する 項番11
+		// System.out.print("電話番号：");
+		// System.out.println(taro.getBangou());
+		//
+		// // 会社名、型番号、契約者名を表示する 項番12
+		// System.out.print("インスタンス[taro]の情報：");
+		// System.out.println(taro.KAISYA + " " + taro.getKataban() + " "
+		// + taro.getKeiyaku());
 
 	}
 
--- src/main/java/pack1/KeitaiDenwa.java
+++ src/main/java/pack1/KeitaiDenwa.java
@@ -6,6 +6,18 @@ public class KeitaiDenwa extends Denwa {
 	private int packetRyokin = 0; // パケット料金
 	public final int PACKET_TANKA = 1; // パケットあたりの加算額
 
+	@Override
+	public void clearRyokin() {
+		super.clearRyokin();
+		this.packetRyokin = 0;
+	}
+
+	@Override
+	public void denwaSuru() {
+		System.out.print("アドレス帳で探しました --> ");
+		super.denwaSuru();
+	}
+
 	public KeitaiDenwa(String keiyaku, String bangou, String mailAddress) {
 		super(keiyaku, bangou); // 契約者名と番号は親のコンストラクタで設定
 		this.mailAddress = mailAddress; // メールアドレスは自分で設定
@@ -19,12 +31,16 @@ public class KeitaiDenwa extends Denwa {
 	}
 
 	public int mail(String joutai) {
-
+		System.out.println("メールを" + joutai + "しました");
+		this.packetRyokin = this.packetRyokin + PACKET_TANKA * 5; // パケット料金の加算
 		return this.packetRyokin; // パケット料金を戻す
 	}
 
 	public int mail(String joutai, int kaisu) {
-
+		for (int i = 0; i < kaisu; i++) {
+			System.out.println((i + 1) + "通目のメールを" + joutai + "しました");
+			this.packetRyokin = this.packetRyokin + PACKET_TANKA * 5; // パケット料金の加算
+		}
 		return this.packetRyokin; // パケット料金を戻す
 	}
 
